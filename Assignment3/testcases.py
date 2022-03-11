65b6b0efb15efbccf9405b487dc9f15342b330f5
Sairaghav1999 <viravalli.s@northeastern.edu>
2022-03-11 11:43:23 -0500

    Update test cases.py

---
+++ Assignment3/testcases.py
@@ -0,0 +1,129 @@
+#PASSED TEST CASES
+{
+  "latitude": 42.253862,
+  "longitude": -122.844200,
+  "date": "2019-04-08",
+  "time": "15:00",
+  "city": "DESCHUTES",
+  "state": "OREGON"
+}
+
+{
+  "latitude": 30.964366,
+  "longitude": -106.283996,
+  "date": "2019-06-02",
+  "time": "17:08",
+  "city": "EDDY",
+  "state": "NEW MEXICO"
+}
+
+{
+  "latitude": 45.430720,
+  "longitude": -107.359613,
+  "date": "2019-06-27",
+  "time": "23:40",
+  "city": "DAWSON",
+  "state": "MONTANA"
+}
+
+{
+  "latitude": 43.072650,
+  "longitude": -97.458831,
+  "date": "2019-06-04",
+  "time": "13:50",
+  "city": "RENVILLE",
+  "state": "MINNESOTA"
+}
+{
+  "latitude": 31.799470,
+  "longitude": -102.312969,
+  "date": "2019-05-20",
+  "time": "18:38",
+  "city": "KING",
+  "state": "TEXAS"
+}
+{
+  "latitude": 36.848225,
+  "longitude": -98.382086,
+  "date": "2019-03-29",
+  "time": "10:19",
+  "city": "LYON",
+  "state": "KANSAS"
+}
+{
+  "latitude": 38.858459,
+  "longitude": -100.333190,
+  "date": "2019-06-14",
+  "time": "19:34",
+  "city": "CLAY",
+  "state": "NEBRASKA"
+}
+{
+  "latitude": 28.202347,
+  "longitude": -87.212569,
+  "date": "2019-06-01",
+  "time": "14:38",
+  "city": "FRANKLIN",
+  "state": "FLORIDA"
+}
+{
+  "latitude": 40.434580,
+  "longitude": -116.243310,
+  "date": "2019-05-07",
+  "time": "15:00",
+  "city": "TWIN FALLS",
+  "state": "IDAHO"
+}
+{
+  "latitude": 35.015741,
+  "longitude": -96.067844,
+  "date": "2019-06-26",
+  "time": "15:32",
+  "city": "BARRY",
+  "state": "MISSOURI"
+}
+#FAILED TEST CASES
+{
+  "latitude": null,
+  "longitude": null,
+  "date": "2019-06-21",
+  "time": "06:44",
+  "city": "JOHNSON",
+  "state": "KANSASS"
+}
+
+{
+  "latitude": null,
+  "longitude": null,
+  "date": "2019-05-16",
+  "time": "16:20",
+  "city": "HENDRIKS",
+  "state": "INDIANA"
+}
+
+{
+  "latitude": null,
+  "longitude": null,
+  "date": "2019-06-23",
+  "time": "18:00",
+  "city": "MIDLAND",
+  "state": "TEXAS"
+}
+
+{
+  "latitude": 34.35504,
+  "longitude": -86.897727,
+  "date": "2019-06-24",
+  "time": "16:00",
+  "city": "ROANE",
+  "state": "TENNESSEE"
+}
+
+{
+  "latitude": 40.594269,
+  "longitude": -99.948691,
+  "date": "2019-06-09",
+  "time": "15:24",
+  "city": "LINN",
+  "state": "IOWA"
+}
