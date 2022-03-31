d1e3549a1ea9be2bdf54ae14a043710cf9b3fda9
Sairaghav1999 <viravalli.s@northeastern.edu>
2022-03-31 13:12:12 -0400

    Add JWT Authentication

--- Assignment4.1/src/scripts/main.py
+++ Assignment4.1/src/scripts/main.py
@@ -3,7 +3,7 @@ from numpy import double, empty
 from geopy.geocoders import Nominatim
 import geopy
 from sqlite3 import Date
-from fastapi import FastAPI, Depends,HTTPException
+from fastapi import FastAPI, Depends,HTTPException,Body
 from pydantic import BaseModel
 from typing import Any, Optional, List
 from make_nowcast_dataset import generate_data
@@ -15,6 +15,11 @@ from fastapi.responses import FileResponse
 from nowcast_data import run
 from google.cloud import storage
 from PIL import Image
+from auth.model import  UserSchema, UserLoginSchema
+from auth.auth_bearer import JWTBearer
+from auth.auth_handler import signJWT
+
+import csv
 
 app = FastAPI()
 
@@ -30,11 +35,19 @@ class Sevir(BaseModel):
     threshold_time: str
     SearchBy: str
 
-
+users = []
    
 @app.get("/")
 def read_root():
     return {"Initialize message": "Welcome to Nowcast API"}
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+
 
 def search_by_loc(date,time,city,state):
     fs=gcsfs.GCSFileSystem(project="sevir-project-bdia",token="cloud_storage_creds.json")
@@ -90,7 +103,7 @@ def search_by_lat_long(latitude,longitude,distancelimit):
         raise HTTPException(status_code=407, detail="Please pass valid values for Lat,Long and DistanceLimit")
 
 
-@app.post('/input/')
+@app.post('/input/',dependencies=[Depends(JWTBearer())], tags=["posts"])
 async def create_sevir_view(sevir: Sevir):
 
     SearchBy=sevir.SearchBy
@@ -126,9 +139,83 @@ async def create_sevir_view(sevir: Sevir):
 
 
 
-   
+@app.post('/input/airflow/',include_in_schema=False)
+async def create_sevir_view(sevir: Sevir):
+
+    SearchBy=sevir.SearchBy
+    refresh_flag=sevir.refresh_flag
+    threshold_time=sevir.threshold_time
+    #Case 1: Search by Location (No Cache logic)
+    if ((SearchBy == 'Loc')):
+        return search_by_loc(sevir.date,sevir.time,sevir.city,sevir.state)
+    #Case 2 : Search by latlong and refresh is Y - Hit model
+    elif((SearchBy == 'LatLong') &(refresh_flag=='Y')):
+        return search_by_lat_long(sevir.latitude,sevir.longitude,sevir.distancelimit)
+    #Case 3 : Search by Latlong and refresh is N - Check Cache first for matching point.
+    elif((SearchBy=='LatLong')&(refresh_flag=='N')):
+        FoundInCache,timestamp,fileloc = searchincache(sevir.latitude,sevir.longitude,sevir.distancelimit)
+        #Found in Cache, check for threshold
+        if(FoundInCache=='Y'):
+            now  = datetime.now()                         
+            duration = now - timestamp                        
+            duration_min = duration.total_seconds()/60
+            #If threshold is okay, return image
+            if(duration_min<=sevir.threshold_time):
+                return fileloc
+            #if Above threshold - hit model
+            else:
+                return search_by_lat_long(sevir.latitude,sevir.longitude,sevir.distancelimit)
+        #Not found in cache - so hit model
+        else:
+            return search_by_lat_long(sevir.latitude,sevir.longitude,sevir.distancelimit)
+    #Fell through all input possibilities
+    else:
+            raise HTTPException(status_code=408, detail="Full fallthrough")
 
 
 
 
+
+@app.post("/user/signup", tags=["user"])
+async def create_user(user: UserSchema = Body(...)):
+    users.append(user) # replace with db call, making sure to hash the password first
+    #print(list(user))   
+    user1=dict(user)
+    #print(dict(user))
+    #print(user1.values())
+    #print(signJWT(user.email))
+    p=signJWT(user.email)
+    #d={'token':p}
+    user1.update(p)
+    #user1.append(p)
+    #print(list(user1.values()))
+    c=list(user1.values())
+    #print(c)
+    print(user1)
+    #user.append(p)
+    fields=['fullname','email','password','access_token']
+    with open('user_details.csv', 'w') as f: 
+        Dict_obj=csv.DictWriter(f, fieldnames=fields)
+        Dict_obj.writerow(user1)
+        f.close()
+        # write = csv.writer(f) 
+        # write.writerow(fields) 
+        # #append_list_as_row('user_details.csv', c)
+        # write.writerows(c) 
+    client = storage.Client.from_service_account_json('cloud_storage_creds.json')
+    bucket=client.bucket('sevir-data-2')
+    blob=bucket.blob('user_details.csv')
+    blob.upload_from_filename('user_details.csv')
+    #print(p)
+    return p
+
+
+
+@app.post("/user/login", tags=["user"])
+async def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
+    }
 
